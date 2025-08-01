652166f0852f84134e539610b96e3dedd7300d89
asifhasan002 <asifannada@gmail.com>
2025-08-01 14:09:40 +0600

    initial commit

---
+++ Memory-Match-Ultimate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memory-Match-Ultimate
+//
+//  Created by Md Asif Hasan on 1/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memory-Match-Ultimate/Memory_Match_UltimateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_Match_UltimateApp.swift
+//  Memory-Match-Ultimate
+//
+//  Created by Md Asif Hasan on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Memory_Match_UltimateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
